28ab2d7755e5fd96c74f2fb84174dce44c688257
howmanysmall <lhamil29@bruinmail.slcc.edu>
2020-07-09 16:56:58 -0600

    Update Configuration.lua

--- src/ReplicatedStorage/RoactMaterial/Configuration.lua
+++ src/ReplicatedStorage/RoactMaterial/Configuration.lua
@@ -1,5 +1,5 @@
 local Configuration = {
-	Promise = require(script.Parent.Parent.ePromise),
+	Promise = require(script.Parent.Parent.Promise),
 	RoactAnimate = require(script.Parent.Parent.RoactAnimate),
 	Roact = require(script.Parent.Parent.Roact),
 	enumerate = require(script.Parent.Parent.enumerate),
